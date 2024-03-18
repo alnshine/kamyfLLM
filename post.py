dd87b8503cd4957efff52fc6d4c4b1a3c9fc2d08
adamauthor <ahmediarolzasov@gmail.com>
2024-03-19 02:21:22 +0500

    add Dockerfile

--- post.py
+++ post.py
@@ -8,10 +8,8 @@ def process_json():
     if request.method == "POST":
         try:
             json_data = request.json
-            print(json_data)
             response_data = model(json_data)
             # Возвращаем созданный JSON-файл
-            print(response_data)
             return response_data, 200
         except Exception as e:
             return jsonify({"error": str(e)}), 400
